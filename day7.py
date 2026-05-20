a4822e553d049ff2c8743d4af242ca542a0501e3
chandini <chandinikandregula5@gmail.com>
2026-05-20 15:33:59 +0530

    Add files via upload

---
+++ day7.py
@@ -0,0 +1,50 @@
+'''
+conditional statements
+----------------------
+if ----> to check the statement is true or not.
+if-else--> else in the if statement,incase and condition becomes false then it will enter into flow back(else),it will execute whatever inside it.
+nested if
+elif
+f string and doc string also same.
+
+'''
+'''
+num = 5
+if num % 2 == 0:
+    print(even)
+
+age_ = int(input("enter your age :"))
+if age_>=18:
+    print(" we are eligible to vote ")
+else:
+    print(f" we have to wait for {"18_ age"} years more")
+'''
+'''
+year_=2000
+if(year_ % 4 == 0 and year_ % 100 != 0) or (year_ % 400 )== 0:
+    print(f"{year_}is a leap year")
+else:
+    print(f"{year_}is not a leap year")
+
+num = -9
+if num >=0:
+   print(f"{num}is a positive number ")
+else:
+    print(f"{num}is a negative number")
+
+ marks_ = int (input("enter your marks:"))
+ stu_name = input("enter your name: ")
+ if marks_>=45:
+     print(f"{stu_name}your passesd")
+ else:
+ print(f"{stu_name}your failed")
+'''
+
+signal_=int(input("enter1.green /n 2.red"))
+if signal_==1:
+    print("lets go")
+else:
+    print("stop")
+
+       
+          
